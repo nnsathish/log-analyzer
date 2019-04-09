401b731a141162eea22ffc8509cf05881bef1976
Sathishkumar Natesan <nnsathish@gmail.com>
2019-04-09 13:14:34 +0530

    Initial gem template

---
+++ lib/log_analyzer.rb
@@ -0,0 +1,6 @@
+require "log_analyzer/version"
+
+module LogAnalyzer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/log_analyzer_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LogAnalyzer do
+  it "has a version number" do
+    expect(LogAnalyzer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
